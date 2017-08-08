1d83a8398c24e36320fee79ce428f29b6a4ddc9b
kong6670 <30824358+kong6670@users.noreply.github.com>
2017-08-08 15:58:09 +0700

    Add setQuackBehavior on Duck
    Test with TestDriver

--- src/Duck.java
+++ src/Duck.java
@@ -8,6 +8,9 @@ public abstract class Duck {
 	void performFly(){
 		flybehavior.fly();
 	}
+	void setQuackBehavior(Quack qb){
+		quackbehavior = qb;
+	}
 	void swim(){
 		System.out.println("I'm swimming");
 	}
