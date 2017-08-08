ca34429ed027e4eab0f745331ba8e2ce660488b9
kong6670 <30824358+kong6670@users.noreply.github.com>
2017-08-08 16:14:49 +0700

    Add setflybehavior on Duck
    Test

--- src/Duck.java
+++ src/Duck.java
@@ -11,6 +11,9 @@ public abstract class Duck {
 	void setQuackBehavior(Quack qb){
 		quackbehavior = qb;
 	}
+	void setflybehavior(Flybehavior fb){
+		flybehavior = fb;
+	}
 	void swim(){
 		System.out.println("I'm swimming");
 	}
--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		System.out.println("----MallardDuck-----");
 		MallardDuck md = new MallardDuck();
 		md.setQuackBehavior(new Quack());
+		md.setflybehavior(new FlyWithRock());
 		md.display();//Look like a mallard
 		md.performQuack();//Quack! Quack!
 		md.performFly();//I'm flying with wing
